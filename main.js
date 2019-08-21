f760e658869b777974aef185cd72a43e44467ab0
Catherine J <cjarocki@gmail.com>
2019-08-20 21:12:44 -0500

    fix b4 merge

--- main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#card-header-about-me").click(function() {
-        $("#plus-minus").fadeOut(100, function() {
-            $("#plus-minus").text(($("#plus-minus").text() == 'About Me ++') ? 'About Me --' : 'About Me ++').fadeIn(100);
-        })
-    })
-});
